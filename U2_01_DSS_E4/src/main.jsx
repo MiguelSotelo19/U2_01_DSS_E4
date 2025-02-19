31f612c9c6d7ade82870697b33944b82dedadad8
MiguelSotelo19 <20223TN113@utez.edu.mx>
2025-02-19 17:59:39 -0600

    Cambios iniciales

--- U2_01_DSS_E4/src/main.jsx
+++ U2_01_DSS_E4/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
+import Login from './screens/Login.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <Routes>
+      <Route path='/' element={<Login />} />
+    </Routes>
+  </Router>
 )
